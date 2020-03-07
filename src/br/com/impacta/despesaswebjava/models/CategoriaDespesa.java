ce5b15dc40d0f16e61ae6700056b51ca17314deb
Rafael Thomazelli <rafael.thomazelli@gmail.com>
2020-03-06 22:37:42 -0300

    conclusão exercício 2

---
+++ src/br/com/impacta/despesaswebjava/models/CategoriaDespesa.java
@@ -0,0 +1,6 @@
+package br.com.impacta.despesaswebjava.models;
+
+public enum CategoriaDespesa {
+	ALIMENTACAO, CUIDADOS_PESSOAIS, DIVERSOS, DOACOES, EDUCACAO, LAZER,
+	MORADIA, SAUDE, SUPERMERCADO, TRANSPORTE, VEICULO, VESTUARIO, VIAGEM
+}
